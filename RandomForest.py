2899412ba1bd473365f02b70fbd758faa0abab45
Giridhar Peddabuttaiahgari <2giridhar.reddy@gmail.com>
2016-11-21 12:41:19 -0700

    random forest testing error

--- RandomForest.py
+++ RandomForest.py
@@ -3,20 +3,30 @@ import numpy as np
 import random
 
 
+def get_train_test_data(in_data, records_count):
+    random.shuffle(in_data)
+    test_data = in_data[:records_count]
+    train_data = in_data[records_count+1:]
+    return train_data, test_data
+
+
 if __name__ == "__main__":
     rf = RandomForestClassifier(n_jobs=2)
     inp_file = raw_input("Enter the input file path: ")
     in_data = np.loadtxt(inp_file, dtype=int, delimiter=',')
-    target = in_data[:,-1]
-    train = in_data[:,[x for x in range(0,22)]]
-    rf.fit(train, target)
-    len = 1000
-    test_data = np.asarray(random.sample(in_data,len))
-    test = test_data[:,[x for x in range(0,22)]]
-    result = rf.predict(test)
-    correct = 0
-    for i in range(0,len):
-        if test_data[i][22] == result[i]:
-            correct += 1
-
-    print(float(correct)/len)*100
+    for i in range(0,5):
+        print("Test Sample: " + str(i))
+        records_count = 100
+        train_data, test_data = get_train_test_data(in_data, records_count)
+        target = train_data[:,-1]
+        train = train_data[:,[x for x in range(0,22)]]
+        rf.fit(train, target)
+        test = test_data[:,[x for x in range(0,22)]]
+        result = rf.predict(test)
+        correct = 0
+        for i in range(0,records_count):
+            if test_data[i][22] == result[i]:
+                correct += 1
+        print("Accuracy:")
+        print(float(correct)/records_count)*100
+        print("___________________")
