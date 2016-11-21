df9da27ab0ed27af6dcee8aa2d64acddaf2aa980
Giridhar Peddabuttaiahgari <2giridhar.reddy@gmail.com>
2016-11-21 11:58:19 -0700

    random forest code fixes

--- RandomForest.py
+++ RandomForest.py
@@ -10,6 +10,13 @@ if __name__ == "__main__":
     target = in_data[:,-1]
     train = in_data[:,[x for x in range(0,22)]]
     rf.fit(train, target)
-    test_data = random.sample(in_data,10)
+    len = 1000
+    test_data = np.asarray(random.sample(in_data,len))
     test = test_data[:,[x for x in range(0,22)]]
-    print rf.predict(test)
+    result = rf.predict(test)
+    correct = 0
+    for i in range(0,len):
+        if test_data[i][22] == result[i]:
+            correct += 1
+
+    print(float(correct)/len)*100
